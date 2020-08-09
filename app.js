0136fded51c15110e1da76c4f8d6665ff7827d11
lucasfraulin <lucasfraulingit@gmail.com>
2020-08-09 11:09:22 -0600

    local host working

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+// root route
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+
+// listening on local host port 3000
+app.listen(3000, function(){
+  console.log("Server running on port 3000");
+});
