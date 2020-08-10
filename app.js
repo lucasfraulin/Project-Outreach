bc7edc892c77246d150185d0190e9c462544e11b
lucasfraulin <lucasfraulingit@gmail.com>
2020-08-10 14:06:12 -0600

    home page content complete

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// about us
+app.get("/about-us", function(req,res){
+  res.render("about-us");
+})
+
 
 // root route
 app.get("/", function(req,res){
